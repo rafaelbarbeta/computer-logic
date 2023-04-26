e65b5e3eb7366767ea7183da4547aacba1ddc912
EriikGabriel <erikgabriel.lins@hotmail.com>
2023-04-25 22:28:41 -0300

    feat: create input functionalities and restricts

--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -1,126 +1,141 @@
+"use client";
+
 import { ArrowLeft, ArrowRight, Delete } from "lucide-react";
 import Image from "next/image";
+import { MouseEvent } from "react";
 import { Key } from "./Key";
 
 export function Keyboard() {
+  function handleKeyPress(e: MouseEvent) {
+    const input = document.querySelector(".search") as HTMLInputElement;
+    const key = (e.currentTarget as HTMLButtonElement).value;
+
+    const start = input.selectionStart ?? 0;
+    const end = input.selectionEnd ?? 0;
+
+    input.value = input.value.slice(0, start) + key + input.value.slice(end);
+    input.focus();
+    input.selectionStart = input.selectionEnd = start + key.length;
+  }
+
   return (
     <div className="fixed flex justify-center items-center bottom-0 h-72 w-2/6 bg-slate-800 rounded-t-lg shadow-md">
       <div className="grid grid-rows-3 grid-cols-5 gap-5 items-center justify-items-center p-10">
-        <Key>
+        <Key onClick={handleKeyPress} value="∧">
           <Image
             src="/assets/and.png"
             alt="AND operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="∨">
           <Image
             src="/assets/or.png"
             alt="OR operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="·">
           <Image
             src="/assets/and-alt.png"
             alt="AND alt operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="+">
           <Image
             src="/assets/or-alt.png"
             alt="OR operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key actionKey>
+        <Key onClick={handleKeyPress} value="(" actionKey>
           <Image
             src="/assets/left-parenthesis.png"
             alt="Left parenthesis"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="⟶">
           <Image
             src="/assets/conditional.png"
             alt="Conditional operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="⟷">
           <Image
             src="/assets/biconditional.png"
             alt="Biconditional operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="¬">
           <Image
             src="/assets/not.png"
             alt="Not operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="⊕">
           <Image
             src="/assets/xor.png"
             alt="XOR operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key actionKey>
+        <Key onClick={handleKeyPress} value=")" actionKey>
           <Image
             src="/assets/right-parenthesis.png"
             alt="Right parenthesis"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="⟹">
           <Image
             src="/assets/implication.png"
             alt="Implication operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key>
+        <Key onClick={handleKeyPress} value="⟺">
           <Image
             src="/assets/equivalence.png"
             alt="Equivalence operation"
             height={24}
             width={24}
-            className="brightness-0 invert"
+            className="brightness-0 invert pointer-events-none"
           />
         </Key>
-        <Key actionKey>
+        <Key onClick={handleKeyPress} value="arrow-left" actionKey>
           <ArrowLeft />
         </Key>
-        <Key actionKey>
+        <Key onClick={handleKeyPress} value="arrow-right" actionKey>
           <ArrowRight />
         </Key>
-        <Key actionKey>
+        <Key onClick={handleKeyPress} value="delete" actionKey>
           <Delete />
         </Key>
       </div>
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,13 +1,89 @@
+"use client";
+
 import { ChevronRight } from "lucide-react";
+
+import { Fira_Code } from "next/font/google";
+import { KeyboardEvent } from "react";
 import { Button } from "../Button";
+
+const firaCode = Fira_Code({ subsets: ["latin"] });
+
+function insertInCursor(chr: string) {
+  const input = document.querySelector(".search") as HTMLInputElement;
+
+  const start = input.selectionStart ?? 0;
+  const end = input.selectionEnd ?? 0;
+
+  input.value = input.value.slice(0, start) + chr + input.value.slice(end);
+  input.focus();
+  input.selectionStart = input.selectionEnd = start + chr.length;
+}
+
+function handleInputEntry(e: KeyboardEvent) {
+  const input = document.querySelector(".search") as HTMLInputElement;
+
+  const shortcutsValues = [
+    "AND",
+    "XOR",
+    "OR",
+    "NOT",
+    "BCOND",
+    "COND",
+    "EQUIV",
+    "IMPL",
+  ];
+
+  const char = e.key;
+  const allowedChars = new RegExp("[A-Za-z0-1() +\b]+");
+  if (!char.match(allowedChars)) e.preventDefault();
+
+  shortcutsValues.forEach((shortcut) => {
+    if (input.value.toUpperCase().includes(shortcut)) {
+      switch (shortcut) {
+        case "AND":
+          insertInCursor("∧");
+          break;
+        case "OR":
+          insertInCursor("∨");
+          break;
+        case "COND":
+          insertInCursor("⟶");
+          break;
+        case "XOR":
+          insertInCursor("⊕");
+          break;
+        case "NOT":
+          insertInCursor("¬");
+          break;
+        case "BCOND":
+          insertInCursor("⟷");
+          break;
+        case "IMPL":
+          insertInCursor("⟹");
+          break;
+        case "EQUIV":
+          insertInCursor("⟺");
+          break;
+        default:
+          break;
+      }
+
+      const start = input.selectionStart ?? 0;
+      input.value = input.value.toUpperCase().replace(shortcut, "");
+      input.selectionStart = input.selectionEnd = start - shortcut.length;
+    }
+  });
+}
 
 export function Search() {
   return (
-    <div className="flex justify-center w-full">
+    <div className={`${firaCode.className} flex justify-center w-full`}>
       <input
         type="text"
         placeholder="Digite a expressão..."
-        className="border-2 border-r-0 text-xl border-slate-800 outline-none bg-inherit p-5 px-8 w-1/2 rounded-l-lg"
+        className="search border-2 border-r-0 text-2xl uppercase placeholder:normal-case border-slate-800 outline-none bg-inherit p-5 px-8 w-1/2 rounded-l-lg"
+        onKeyUp={handleInputEntry}
+        onKeyPress={handleInputEntry}
       />
       <Button className="rounded-s-none">
         <ChevronRight size={32} />
