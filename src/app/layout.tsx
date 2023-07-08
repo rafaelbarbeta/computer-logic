a5a9d8353c73baceaa901d5090447c4bf14d0828
EriikGabriel <erikgabriel.lins@hotmail.com>
2023-07-08 00:21:37 -0300

    feat: add dynamic favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
-import { Menu } from "@components/Menu";
-import { ExpressionContextProvider } from "@contexts/ExpressionContext";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Menu } from "@components/Menu"
+import { ExpressionContextProvider } from "@contexts/ExpressionContext"
+import { Inter } from "next/font/google"
+import Head from "next/head"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Lógica Computacional",
   description: "Calculadora de lógica computacional",
-};
+  icons: {
+    icon: "/favicon.svg",
+  },
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="pt-br">
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body className={inter.className}>
         <ExpressionContextProvider>
           <Menu />
@@ -24,5 +31,5 @@ export default function RootLayout({
         </ExpressionContextProvider>
       </body>
     </html>
-  );
+  )
 }
